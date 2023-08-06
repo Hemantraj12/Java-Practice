bd164b171b8f96e0720e64294aa2c6a62f7bf527
Hemantraj12 <141589145+Hemantraj12@users.noreply.github.com>
2023-08-07 01:01:22 +0300

    myfirstcommit

---
+++ src/Main.java
@@ -0,0 +1,88 @@
+
+
+
+
+
+
+
+
+
+
+//            String sc = "Example.com";
+//            int a = sc.length();
+//         System.out.println("The string length of '"+sc+"' is: "+a);
+
+
+//        String str1 = "Shanghai Houston colorado Alexandria";
+//        String str2 = "Alexandria Colorado Houston Shanghai";
+
+//        boolean match1 = str1.regionMatches(0, str2, 28, 8);
+//        boolean match2 = str1.regionMatches(9,str2, 9,8);
+//        System.out.println("str1[0 - 7] == str2[28 - 35]? " + match1);
+//        System.out.println("str1[9 - 15] == str2[9 - 15]? " + match2);
+
+//        String a = "The quick brown fox jumps over the lazy dog";
+//        String b = a.replace('d', 'f');
+//        System.out.println("Original String: " + a);
+//        System.out.println("New String: " +b);
+
+//        String a = "The quick brown fox jumps over the lazy dog.";
+//        String b = a.replaceAll("fox", "cat");
+//        System.out.println("Original string: " +a);
+//        System.out.println("New String: " +b);
+
+//        String a = "Red is favorite color.";
+//        String b = "Orange is also my favorite color.";
+//        String startStr = "Red";
+//
+//        boolean starts1 = a.startsWith(startStr);
+//        boolean starts2 = b.startsWith(startStr);
+//        System.out.println( a + " starts with " + startStr + "?" + starts1);
+//        System.out.println( b + " starts with " + startStr + "?" + starts2);
+
+
+//        String a = "Red is favorite color.";
+//        System.out.print("Red is favorite color. starts with Red? ");
+//        System.out.println(a.startsWith("Red"));
+//        String b = "Orange is also my favorite color";
+//        System.out.print("Orange is also my favorite color? ");
+//        System.out.println(b.startsWith("Red"));
+
+//        String a = "The quick brown fox jumps over the lazy dog.";
+//        String b = a.substring(10,26);
+//        System.out.println("old = " + a);
+//        System.out.println("new = " + b );
+//public class Main {
+//    public static void main(String[] args) {
+//        String a = "java Exercise";
+//        char[] arr = a.toCharArray();
+//        System.out.println(arr);
+//public class Main{
+//    public static void main(String[] args) {
+//        String a = "The Quick BroWn FoX!";
+//        String b = a.toLowerCase();
+//        System.out.println("Original String: " +a);
+//        System.out.println("String in lowercase: " +b);
+//    }
+//    }
+
+//        String a = "The Quick BroWn FoX!";
+//        String b = a.toUpperCase();
+//        System.out.println("Original String: " +a);
+//        System.out.println("String in uppercase: " +b);
+
+
+
+//        String a = "Java Exercises";
+//        String b = a.trim();
+//        System.out.println("Original String: " + a);
+//        System.out.println("New String: " + b);
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
---
+++ src/array.java
@@ -0,0 +1,315 @@
+import java.util.*;
+import java.util.Scanner;
+
+//        int[] marks = new int[3];
+       // int marks[] = new int[3];
+//        int marks[] = {97, 65, 98};
+//        marks[0] = 98;
+//        marks[1] = 94;
+//        marks[2] = 95;
+//        System.out.println(marks[0]);
+//        System.out.println(marks[1]);
+//        System.out.println(marks[2]);
+//        Scanner sc = new Scanner(System.in);
+//        int size = sc.nextInt();
+//        int numbers[] = new int[size];
+//        //input
+//        for(int i=0; i<size; i++) {
+//            numbers[i] = sc.nextInt();
+//        }
+//
+//        //ouput
+//        for (int i = 0; i < size; i++) {
+//            System.out.println(numbers[i]);
+
+//        double x = 2.00000;
+//        int n = 10;
+//        double a = (x/n);
+//        System.out.println(a);
+
+
+//                int[] marks = {98, 65, 85, 74, 55};
+//                System.out.println(marks.length);
+//                System.out.println(marks[3]);
+
+//                String [] students = {"basant", "sunny", "munny", "Ashu", "Hemant"};
+////                System.out.println(students.length);
+////                System.out.println(students[3]);
+//                System.out.println("in proper order");
+//                for(int i = 0; i<5; i++) {
+//                    System.out.println(students[i]);
+//                }
+//                System.out.println("in reverse order");
+//                    for(int i = 5 -1; i>=0; i--) {
+//                        System.out.println(students[i]);
+//
+//                        for(String element: students) {
+//                            System.out.println(element);
+//                        }
+//                }
+
+
+
+
+//        int [] marks = new int[5];
+////        int [] marks;
+////        marks = new int[5];
+//        marks[0] = 100;
+//        marks[1] = 70;
+//        marks[2] = 60;
+//        marks[3] = 90;
+//        marks[4] = 86;
+//        for(int i = 0; i<5; i++){
+//            System.out.println(marks[i]);
+//        System.out.println("markss");
+//        int[] marks = {98, 68, 47, 82, 98};
+//        System.out.println(marks.length);
+//        for(int i = 0; i<5; i++){
+//            System.out.println(marks[i]);
+//        }
+//        System.out.println("name");
+//        for(int i = 0; i<5; i++){
+//            System.out.println(marks[i]);
+//        String[] students = {"hemant", "ashu", "basant", "munny", "sunny"};
+//        System.out.println(students.length);
+//        for (int p=students.length -1;p>=0; p--) {
+//            System.out.println(students[p]);
+//            for (int p= 5 -1; p>=0; p--) {
+//                System.out.println(students[p]);
+        // }
+        //foreach
+//        for(String element: students) {
+//            System.out.println(element);
+//
+//            int marks[] = new int[3];
+//
+//            marks[0]= 95;
+//            marks[1]= 98;
+//            marks[2]= 91;
+//            System.out.println(marks[0]);
+//            System.out.println(marks[2]);
+
+//        int[][] flats;
+//        flats = new int[2][3];
+//        flats[0][0] = 101;
+//        flats[0][1] = 102;
+//        flats[0][2] = 103;
+//        flats[1][0] = 201;
+//        flats[1][1] = 202;
+//        flats[1][2] = 203;
+//        System.out.println("printing a 2-D array");
+//        for(int i = 0; i<flats.length; i++) {
+//            for (int j=0; j<flats[i].length;j++) {
+//                System.out.print(flats[i][j]);
+//                System.out.print(" ");
+//            }
+//            System.out.println(" ");
+//        }
+
+
+        //practice1
+//        float [] number = { 45.5f, 89.5f, 56.78f, 26.45f, 58.265f,};
+//        float sum = 0;
+//        for(float element:number){
+//            sum = sum+element;
+//
+//        }
+//        System.out.println("The value is: " +sum);
+
+
+        //problem2
+//        float[] number = {45.5f, 89.5f, 56.78f, 26.45f, 58.265f,};
+//        float num = 45.5f;
+//        boolean isInArray = false;
+//        for (float element : number) {
+//            if (num == element) {
+//                isInArray = true;
+//                break;
+//            }
+//
+//        }
+//        if (isInArray) {
+//            System.out.println("The value is present in the array");
+//        } else {
+//            System.out.println("The value is not present in the array");
+//        }
+
+
+        //pratice problem3
+
+//                float [] physicsMark = {89.5f, 58.78f, 52.2f, 45.85f, 45.36f};
+//                float sum = 0;
+//                for(float element: physicsMark){
+//                    sum = sum + element;
+//
+//        }
+//        System.out.print("Average of physics mark is: " +sum/physicsMark.length);
+
+        //practice problem4;
+//        int[][] matrix1 = {{1, 2, 3},
+//                {4, 5, 6}};
+//        int[][] matrix2 = {{2, 6, 13},
+//                {3, 7, 1}};
+//
+//        int[][] result = {{0, 0, 0,},
+//                           {0, 0, 0}};
+//        for (int i = 0; i < matrix1.length; i++) {
+//            for (int j = 0; j < matrix1[i].length; j++) {
+//                System.out.format(" Setting value for i=%d and j=%d\n", i, j);
+//                result[i][j] = matrix1[i][j] + matrix2[i][j];
+//            }
+//        }
+//        for (int i = 0; i < matrix1.length; i++) {
+//            for (int j = 0; j < matrix1[i].length; j++) {
+//                System.out.print(result[i][j] + " ");
+//                result[i][j] = matrix1[i][j] + matrix2[i][j];
+//            }
+//            System.out.println("");
+//        }
+//        float[] number = {45.5f, 89.5f, 56.78f, 26.45f, 58.265f,};
+//        System.out.println(number.length);
+//        for (float p =number.length -1; p>=0; p--) {
+//            System.out.println(number[p]);
+//
+//         //   System.out.println(marks[i]);
+////        String[] students = {"hemant", "ashu", "basant", "munny", "sunny"};
+////        System.out.println(students.length);
+////        for (int p=students.length -1;p>=0; p--) {
+////            System.out.println(students[p]);
+//        }
+
+//        int[] arr = { 1, 2, 3, 85, 98, 56, 25};
+//        int l = arr.length;
+//        int n = Math.floorDiv(l,2);
+//        int temp;
+//        for(int i=0; i<n; i++){
+//            temp = arr[i];
+//            arr[i] = arr[l-i-1];
+//            arr[l-i-1] = temp;
+//
+//        }
+//        for(int element: arr)
+//        {
+//            System.out.print(element + " ");
+  //      }
+
+
+//        int[] arr = { 1, 2, 3, 588, 98, 56, 25};
+//        int max = 0; //Interger.MIN_VALUE
+//        for(int e: arr) {
+//            if(e>max) {
+//                max = e;
+//            }
+//        }
+//        System.out.println("The maximum value on this array is: " + max);
+//
+//        int[] prr = { 5, 89, 25, 669, 25,};
+//        int min = 699;
+//        for(int f: prr) {
+//            if(f<min) {
+//               min = f;
+//            }
+//        }
+//        System.out.println("The Lowest value of this array is: " + min);
+
+
+        //    boolean isSorted = true;
+        //        int[] arr = { 1, 2, 3, 588, 98, 56, 25};
+        //        for(int i = 0; i<arr.length-1; i++) {
+        //            if(arr[i] > arr[i+1]) {
+        //                isSorted = false;
+        //                break;
+        //            }
+        //        }
+        //        if(isSorted){
+        //            System.out.println("The Array is sorted");
+        //
+        //        }
+        //        else{
+        //            System.out.println("The Array is not sorted");
+        //        }
+
+public class array {
+    public static void main(String[] args) {
+
+//        float [] marks = {58.3f, 98.47f, 55.98f, 224.78f, 558.47f};
+//        float sum = 0;
+//        for(float element: marks) {
+//            sum = sum + element;
+//
+//        }
+//        System.out.println("The value of sum is: " + sum);
+
+//        float [] marks = {58.3f, 98.47f, 55.98f, 224.78f, 558.47f};
+//        float sum = 55.498f;
+//        boolean isInArray = false;
+//        for(float element: marks) {
+//            if (sum==element) {
+//                isInArray = true;
+//                break;
+//            }
+//        }
+//if(isInArray) {
+//    System.out.println("The value is present in the array: ");
+//
+//}
+//else{
+//    System.out.println("The value is not present in the array: ");
+//}
+
+//            int[] marks = { 89, 58, 69, 58, 25};
+//            int sum = 0;
+//            for(int element:marks) {
+//                sum = sum + element;
+//            }
+//        System.out.println("The average value of the sum is " + sum/marks.length);
+//       // System.out.println(sum/marks.length);
+
+
+//        int[][] mat1 = {{1, 2, 3},
+//                {4, 5, 6}};
+//        int[][] mat2 = {{8, 6, 7},
+//                {3, 8, 9}};
+//        int[][] result = {{0, 0, 0},
+//                {0, 0, 0}};
+//
+//        for (int i = 0; i < mat1.length; i++) {
+//            for (int j = 0; j < mat1[i].length; j++) {
+//                System.out.format(" Setting value for i=%d and j=%d\n", i, j);
+//                result[i][j] = mat1[i][j] + mat2[i][j];
+//            }
+//        }
+//
+//        for (int i = 0; i < mat1.length; i++) {
+//            for (int j = 0; j < mat1[i].length; j++) {
+//                System.out.print(result[i][j] + " ");
+//                result[i][j] = mat1[i][j] + mat2[i][j];
+
+
+//            }
+//            System.out.println(" ");
+//
+
+     //   }
+
+//        int a = 8;
+//         int b = 45;
+//         int c = a+b;
+//        System.out.println(c);
+
+//        Scanner sc= new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = a+b;
+//        System.out.println(c);
+
+
+
+
+
+
+    
+
+
+
+
